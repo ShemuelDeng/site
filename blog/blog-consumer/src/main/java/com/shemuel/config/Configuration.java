44118de4173839a0eb59ab20002ab82e1a1209e0
shemuel <1241916209@qq.com>
2019-04-26 18:11:18 +0800

    项目结构重构

---
+++ blog/blog-consumer/src/main/java/com/shemuel/config/Configuration.java
@@ -0,0 +1,26 @@
+package com.shemuel.config;
+
+import com.shemuel.interceptor.HandleInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+// webmvcConfigurer 接口替换了以前的webmvcConfigurAdapter类
+@org.springframework.context.annotation.Configuration
+public class Configuration extends WebMvcConfigurerAdapter {
+    @Autowired
+    private HandleInterceptor handleInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 取消对静态资源的拦截
+        registry.addInterceptor(handleInterceptor).addPathPatterns("/*").excludePathPatterns("classpath:/static/");
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
